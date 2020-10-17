8b81a05f295f2a922b42df0185b60c220d6b9ee7
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-10-17 18:54:44 -0400

    adding first function lesson and exercise

---
+++ first_functions/exercise/unsolved/function_exercise.js
@@ -0,0 +1,47 @@
+/*
+• Declare a function that takes any string as an argument and
+  console logs to the user the text they've entered, invoke
+  it after it has been declared
+*/
+//insert code here
+//<------------------------------------------------------->
+
+
+
+//<------------------------------------------------------->
+
+/*
+• Declare a function that takes no arguments but prints something
+  to the console, invoke it after it has been declared
+*/
+//insert code here
+//<------------------------------------------------------->
+
+
+
+//<------------------------------------------------------->
+
+/*
+• Declare a function that takes 2 arguments and returns the value of those 2
+  arguments concatanated together
+*/
+//insert code here
+//<------------------------------------------------------->
+
+
+
+//<------------------------------------------------------->
+
+/*
+• Declare a function that receives an input of door
+  1, 2 or 3. Depending upon which virtual "door" was entered, the
+  function will tell the user they've received a different "prize"
+  in an alert. Try running it after it has been declared a few times
+  with each door option.
+*/
+//insert code here
+//<------------------------------------------------------->
+
+
+
+//<------------------------------------------------------->
